b1dccc8eb35062e84df38a879375482044de5b8b
Gw <3297150980@qq.com>
2022-07-11 15:56:18 +0800

    添加index.html

--- server.js
+++ server.js
@@ -26,11 +26,6 @@ var server = http.createServer(function(request, response){
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(`二哈`)
     response.end()
-  } else if(path === '/x'){
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/css;charset=utf-8')
-    response.write(`body{color: red;}`)
-    response.end()
   } else {
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
